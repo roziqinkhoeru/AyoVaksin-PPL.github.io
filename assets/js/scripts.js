bd1c2bccff6673757f53d38e1b4615b6185b01b0
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2021-12-04 23:58:06 +0700

    add scroll to top all page and fixing icon all page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,3 +4,22 @@ const tooltipsPship = document.querySelectorAll('.vaksin__tt')
 tooltipsPship.forEach(t => {
 	new bootstrap.Tooltip(t)
 })
+
+// scroll to up button
+$(document).ready(function() {
+	$(window).scroll(function() {
+		if($(window).scrollTop() > 720) {
+			$('.scroll__up').css({
+				"opacity":"1", "pointer-events":"auto"
+			});
+		} else {
+			$('.scroll__up').css({
+				"opacity":"0", "pointer-events":"none"
+			});
+		}
+	});
+
+	$('.scroll__up').click(function() {
+		$('html').animate({scrollTop:0}, 150);
+	});
+});
