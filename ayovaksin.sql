bc8242ae39b62cd356884faeb4452a10b1a2b6db
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-08 08:40:02 +0700

    Upload Database

---
+++ ayovaksin.sql
@@ -0,0 +1,329 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 08 Des 2021 pada 02.37
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ayovaksin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `calon_penerima_vaksin`
+--
+
+CREATE TABLE `calon_penerima_vaksin` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nik` int(11) NOT NULL,
+  `kk` int(11) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(6) NOT NULL,
+  `usia` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `no_telp` varchar(13) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `kode_pos` int(11) NOT NULL,
+  `id_login` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kotak_saran`
+--
+
+CREATE TABLE `kotak_saran` (
+  `id_saran` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `no_telp` varchar(13) NOT NULL,
+  `subject` varchar(50) NOT NULL,
+  `pesan` longtext NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pendaftaran`
+--
+
+CREATE TABLE `pendaftaran` (
+  `id_pendaftaran` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `tanggal_pendaftaran` datetime NOT NULL,
+  `id_vaksin` int(11) NOT NULL,
+  `creted_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penyedia_layanan`
+--
+
+CREATE TABLE `penyedia_layanan` (
+  `id_penyedia` int(11) NOT NULL,
+  `nama_instansi` varchar(255) NOT NULL,
+  `no_surat_tugas` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `no_telp` varchar(13) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `kode_pos` int(11) NOT NULL,
+  `id_login` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `role`
+--
+
+CREATE TABLE `role` (
+  `id_role` int(11) NOT NULL,
+  `role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sertifikat`
+--
+
+CREATE TABLE `sertifikat` (
+  `id_sertifikat` int(11) NOT NULL,
+  `no_sertifikat` varchar(255) NOT NULL,
+  `id_tiket` int(11) NOT NULL,
+  `batch_id` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tiket`
+--
+
+CREATE TABLE `tiket` (
+  `id_tiket` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_pendaftaran` int(11) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id_login` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `id_role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `vaksin`
+--
+
+CREATE TABLE `vaksin` (
+  `id_vaksin` int(11) NOT NULL,
+  `nama_vaksin` varchar(255) NOT NULL,
+  `waktu` time NOT NULL,
+  `tanggal_vaksinasi` date NOT NULL,
+  `id_penyedia` int(11) NOT NULL,
+  `kuota` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `calon_penerima_vaksin`
+--
+ALTER TABLE `calon_penerima_vaksin`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `id_login` (`id_login`);
+
+--
+-- Indeks untuk tabel `kotak_saran`
+--
+ALTER TABLE `kotak_saran`
+  ADD PRIMARY KEY (`id_saran`);
+
+--
+-- Indeks untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  ADD PRIMARY KEY (`id_pendaftaran`),
+  ADD KEY `vaksin` (`id_vaksin`);
+
+--
+-- Indeks untuk tabel `penyedia_layanan`
+--
+ALTER TABLE `penyedia_layanan`
+  ADD PRIMARY KEY (`id_penyedia`),
+  ADD KEY `login` (`id_login`);
+
+--
+-- Indeks untuk tabel `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id_role`);
+
+--
+-- Indeks untuk tabel `sertifikat`
+--
+ALTER TABLE `sertifikat`
+  ADD PRIMARY KEY (`id_sertifikat`),
+  ADD KEY `tiket` (`id_tiket`);
+
+--
+-- Indeks untuk tabel `tiket`
+--
+ALTER TABLE `tiket`
+  ADD PRIMARY KEY (`id_tiket`),
+  ADD KEY `user` (`id_user`),
+  ADD KEY `pendaftaran` (`id_pendaftaran`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_login`),
+  ADD KEY `role` (`id_role`);
+
+--
+-- Indeks untuk tabel `vaksin`
+--
+ALTER TABLE `vaksin`
+  ADD PRIMARY KEY (`id_vaksin`),
+  ADD KEY `penyedia` (`id_penyedia`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `calon_penerima_vaksin`
+--
+ALTER TABLE `calon_penerima_vaksin`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `kotak_saran`
+--
+ALTER TABLE `kotak_saran`
+  MODIFY `id_saran` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `penyedia_layanan`
+--
+ALTER TABLE `penyedia_layanan`
+  MODIFY `id_penyedia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `role`
+--
+ALTER TABLE `role`
+  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `sertifikat`
+--
+ALTER TABLE `sertifikat`
+  MODIFY `id_sertifikat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tiket`
+--
+ALTER TABLE `tiket`
+  MODIFY `id_tiket` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `vaksin`
+--
+ALTER TABLE `vaksin`
+  MODIFY `id_vaksin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `calon_penerima_vaksin`
+--
+ALTER TABLE `calon_penerima_vaksin`
+  ADD CONSTRAINT `id_login` FOREIGN KEY (`id_login`) REFERENCES `users` (`id_login`) ON DELETE NO ACTION;
+
+--
+-- Ketidakleluasaan untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  ADD CONSTRAINT `vaksin` FOREIGN KEY (`id_vaksin`) REFERENCES `vaksin` (`id_vaksin`) ON DELETE NO ACTION;
+
+--
+-- Ketidakleluasaan untuk tabel `penyedia_layanan`
+--
+ALTER TABLE `penyedia_layanan`
+  ADD CONSTRAINT `login` FOREIGN KEY (`id_login`) REFERENCES `users` (`id_login`) ON DELETE NO ACTION;
+
+--
+-- Ketidakleluasaan untuk tabel `sertifikat`
+--
+ALTER TABLE `sertifikat`
+  ADD CONSTRAINT `tiket` FOREIGN KEY (`id_tiket`) REFERENCES `tiket` (`id_tiket`) ON DELETE NO ACTION;
+
+--
+-- Ketidakleluasaan untuk tabel `tiket`
+--
+ALTER TABLE `tiket`
+  ADD CONSTRAINT `pendaftaran` FOREIGN KEY (`id_pendaftaran`) REFERENCES `pendaftaran` (`id_pendaftaran`) ON DELETE NO ACTION,
+  ADD CONSTRAINT `user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_login`) ON DELETE NO ACTION;
+
+--
+-- Ketidakleluasaan untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `role` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `vaksin`
+--
+ALTER TABLE `vaksin`
+  ADD CONSTRAINT `penyedia` FOREIGN KEY (`id_penyedia`) REFERENCES `penyedia_layanan` (`id_penyedia`) ON DELETE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
