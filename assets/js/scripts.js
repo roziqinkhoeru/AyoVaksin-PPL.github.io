5f35371c06a9870caf8aa9a0411087eaecba4117
musaalfian <musaganteng071@gmail.com>
2021-12-08 12:22:43 +0700

    periksa-musa

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,25 +1,29 @@
+// zoom
+mediumZoom(".zoom", {});
 // tooltips
 // vaksin
-const tooltipsPship = document.querySelectorAll('.vaksin__tt')
-tooltipsPship.forEach(t => {
-	new bootstrap.Tooltip(t)
-})
+const tooltipsPship = document.querySelectorAll(".vaksin__tt");
+tooltipsPship.forEach((t) => {
+  new bootstrap.Tooltip(t);
+});
 
 // scroll to up button
-$(document).ready(function() {
-	$(window).scroll(function() {
-		if($(window).scrollTop() > 720) {
-			$('.scroll__up').css({
-				"opacity":"1", "pointer-events":"auto"
-			});
-		} else {
-			$('.scroll__up').css({
-				"opacity":"0", "pointer-events":"none"
-			});
-		}
-	});
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 720) {
+      $(".scroll__up").css({
+        opacity: "1",
+        "pointer-events": "auto",
+      });
+    } else {
+      $(".scroll__up").css({
+        opacity: "0",
+        "pointer-events": "none",
+      });
+    }
+  });
 
-	$('.scroll__up').click(function() {
-		$('html').animate({scrollTop:0}, 150);
-	});
+  $(".scroll__up").click(function () {
+    $("html").animate({ scrollTop: 0 }, 150);
+  });
 });
